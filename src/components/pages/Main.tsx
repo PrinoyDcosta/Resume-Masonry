aef65beb4579c8a7672d0fc8b40a3ab75d1e6319
Prinoy D'Costa <78203778+PrinoyDcosta@users.noreply.github.com>
2025-07-10 03:18:14 +0400

    Remove bloat code , updated example to point to npm package

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,21 +1,15 @@
-import { useRef } from "react";
-import ResumeWrapper from "../wrappers/ResumeWrapper"
-import ToolsWrapper from "../wrappers/ToolsWrapper"
+import { useRef, useState } from "react";
+import { dataSource, Resume } from "react-resume-viewer"
+import { ISection } from "node_modules/react-resume-viewer/dist/types/components/common/constants/section-consts";
 
 const Main = () => {
     const printableContentRef = useRef<HTMLDivElement>(null);
+    const [data, setData] = useState<Array<ISection>>(dataSource)
 
     return(<>
     <div className='flex flex-row px-24'>
-        <div className="basis-3/4">
-            <div className="p-40 pt-10">
-                <ResumeWrapper printableContentRef={printableContentRef} />
-            </div>
-        </div>
-        <div className="basis-1/4">
-            <div className="pt-10 ">
-                <ToolsWrapper printableContentRef={printableContentRef} />
-            </div>
+        <div className="p-40 pt-10">
+            <Resume data={data} setData={setData} printableContentRef={printableContentRef} />
         </div>
     </div>
     </>)
